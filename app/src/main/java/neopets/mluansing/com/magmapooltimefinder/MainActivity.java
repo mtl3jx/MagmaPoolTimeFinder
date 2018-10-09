94ff3d6bece0a99b0791a3111ee08b61c4fea61e
Madelyn Luansing <mluansing@captechventures.com>
2018-10-09 18:07:55 -0400

    takes screenshots on page reload

--- app/src/main/java/neopets/mluansing/com/magmapooltimefinder/MainActivity.java
+++ app/src/main/java/neopets/mluansing/com/magmapooltimefinder/MainActivity.java
@@ -1,28 +1,28 @@
 package neopets.mluansing.com.magmapooltimefinder;
 
-import android.content.Context;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -37,9 +37,11 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean isRunning = false;
     final Handler handler = new Handler();
-    private static final int REFRESH_INTERVAL = 1; // TODO: change to 5 mins
+    private static final int REFRESH_INTERVAL = 5;
     private static final String URL_TO_RELOAD = "http://www.neopets.com/magma/pool.phtml";
-    private static final SimpleDateFormat TIMESTAMP_FORMAT = new SimpleDateFormat("HH:mm:ssZ", Locale.US);
+    private static final SimpleDateFormat TIMESTAMP_FORMAT = new SimpleDateFormat("HH:mm:ss", Locale.US);
+
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 121915;
 
     // TODO: allow custom setting of timer
 
@@ -50,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        initializeDownloadPermissions();
+
         fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,14 +61,15 @@ public class MainActivity extends AppCompatActivity {
                 if (isRunning) {
                     Toast.makeText(MainActivity.this, "Turning off auto-refresh timer", Toast.LENGTH_SHORT).show();
 
-                    enableAutoRefreshTimer();
+                    disableAutoRefreshTimer();
 
                     fab.setImageResource(R.drawable.ic_start);
                     isRunning = !isRunning;
                 } else {
                     Toast.makeText(MainActivity.this, "Starting auto-refresh timer for every 5 mins", Toast.LENGTH_SHORT).show();
 
-                    disableAutoRefreshTimer();
+                    enableAutoRefreshTimer();
+
                     fab.setImageResource(R.drawable.ic_stop);
                     isRunning = !isRunning;
                 }
@@ -76,14 +81,20 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
         webView.loadUrl(URL_TO_RELOAD);
+    }
 
-        webView.setWebViewClient(new WebViewClient() {
-            public void onPageFinished(WebView view, String url) {
-                // screenshot after webpage loaded
-                takeScreenshot();
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull final int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
+        if (requestCode == MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // TODO: permission granted to store screenshots -- do nothing?
+            } else {
+                // user refused to grant permission
+                Toast.makeText(MainActivity.this, R.string.download_permission_needed, Toast.LENGTH_LONG).show();
             }
-        });
+        }
     }
 
     @Override
@@ -108,9 +119,33 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void initializeDownloadPermissions() {
+        // ask for run time permission if running sdk 23+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // need user permission
+
+            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // can explain why permissions needed here
+            }
+
+            // ask for permission
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+        } else {
+            // TODO: permission granted to store to device -- do nothing
+        }
+    }
+
     private void enableAutoRefreshTimer() {
+        // screenshot after webpage loaded
+        webView.setWebViewClient(new WebViewClient() {
+            public void onPageFinished(WebView view, String url) {
+                takeScreenshot();
+            }
+        });
+
         // set auto-refresh timer for every 5 mins
-        webView.loadUrl(URL_TO_RELOAD);
+        webView.reload();
 
         reloadWebView();
     }
@@ -119,21 +154,21 @@ public class MainActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Log.d(TAG, "Reloading web page at " +  getTimestamp());
+                Log.d(TAG, "Reloading web page at " + getTimestamp());
 
-                webView.loadUrl(URL_TO_RELOAD);
+                webView.reload();
 
                 // reschedule next timer
                 reloadWebView();
             }
-        }, REFRESH_INTERVAL * 60 * 1000);
+        }, 30 * 1000); // TODO: put refresh interval back
     }
 
     private void takeScreenshot() {
         // generate filename with timestamp
         String timestamp = getTimestamp();
 
-        Log.d(TAG, "Taking screenshot at " +  timestamp);
+        Log.d(TAG, "Taking screenshot at " + timestamp);
 
         // store screenshot
         Bitmap b = Screenshot.takeScreenshotOfRootView(webView);
@@ -141,72 +176,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void storeScreenshot(Bitmap bitmap, String filename) {
-//        ATTEMPT 1
-//        String path = Environment.getExternalStorageDirectory().toString() + "/" + filename;
-//        OutputStream out = null;
-//        File imageFile = new File(path);
-//
-//        try {
-//            out = new FileOutputStream(imageFile);
-//            // choose JPEG format
-//            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
-//            out.flush();
-//        } catch (FileNotFoundException e) {
-//            Log.e(TAG, "FileNotFoundException" + e.getMessage());
-//        } catch (IOException e) {
-//            // manage exception ...
-//            Log.e(TAG, "IOException " + e.getMessage());
-//        } finally {
-//
-//            try {
-//                if (out != null) {
-//                    out.close();
-//                }
-//
-//            } catch (Exception exc) {
-//                Log.e(TAG, exc.getMessage());
-//            }
-//
-//        }
-
-//        ATTEMPT 2
-//        File myDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-//
-//        if (!myDir.exists()) {
-//            myDir.mkdirs();
-//        }
-//
-//        File file = new File (myDir, filename);
-//        if (file.exists ())
-//            file.delete ();
-//        try {
-//            FileOutputStream out = new FileOutputStream(file);
-//            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
-//            out.flush();
-//            out.close();
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
-//        ATTEMPT 3
-        final String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Screenshots";
-        File dir = new File(dirPath);
-        if(!dir.exists())
-            dir.mkdirs();
-        File file = new File(dirPath, filename);
+        // ATTEMPT 4
+        String root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
+        File myDir = new File(root + "/Screenshots/MagmaPoolTimeFinder");
+        myDir.mkdirs();
+        File file = new File(myDir, filename + ".jpg");
+        if (file.exists()) {
+            file.delete();
+        }
         try {
-            FileOutputStream fOut = new FileOutputStream(file);
-            bitmap.compress(Bitmap.CompressFormat.PNG, 85, fOut);
-            fOut.flush();
-            fOut.close();
+            FileOutputStream out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        // Tell the media scanner about the new file so that it is immediately available to the user.
+        MediaScannerConnection.scanFile(this, new String[]{file.toString()}, null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    public void onScanCompleted(String path, Uri uri) {
+                        Log.i("ExternalStorage", "Scanned " + path);
+                        Log.i("ExternalStorage", "-> uri=" + uri);
+                    }
+                });
+
     }
 
     private void disableAutoRefreshTimer() {
+        // remove page reload listener
+        webView.setWebViewClient(null);
+
         // remove future timers
         handler.removeCallbacksAndMessages(null);
     }
