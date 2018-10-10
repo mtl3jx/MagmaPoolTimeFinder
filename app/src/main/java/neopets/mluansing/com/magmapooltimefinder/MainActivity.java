876e535ce5b21fe80bdda6c920b2a088a9878f9f
Madelyn Luansing <mluansing@captechventures.com>
2018-10-09 22:29:42 -0400

    added manual refresh and changed refresh interval to every 5 minutes

--- app/src/main/java/neopets/mluansing/com/magmapooltimefinder/MainActivity.java
+++ app/src/main/java/neopets/mluansing/com/magmapooltimefinder/MainActivity.java
@@ -99,20 +99,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_refresh) {
+            Log.d(TAG, "Manually reloading web page at " + getTimestamp());
+
+            webView.reload();
             return true;
         }
 
@@ -161,7 +159,7 @@ public class MainActivity extends AppCompatActivity {
                 // reschedule next timer
                 reloadWebView();
             }
-        }, 30 * 1000); // TODO: put refresh interval back
+        }, REFRESH_INTERVAL * 60 * 1000);
     }
 
     private void takeScreenshot() {
